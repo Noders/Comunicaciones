9142133ba735ca7b295bea0b749d468b211058fc
Fernando Larrañaga <xabadu@gmail.com>
2017-12-01 16:54:19 -0800

    Add script to autogenerate meetup emails

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Comunicaciones
 Repo para comunicaciones.
+
+## Scripts
+
+### Generación de Emails para Meetups
+
+#### Uso
+
+- Agregar archivo de información del meetup en `data/year-month.json` (Ver `data/2017-12.json` como referencia).
+- Ejecutar `yarn email:pre-meetup -- año-mes` o `yarn email:post-meetup -- año-mes` (Ej. `yarn email:pre-meetup 2017-12`).
+- Profit.
---
+++ scripts/generate-email.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const meetup = require(`../data/${process.argv[3]}`);
+
+const generateEmail = (type, date) => {
+  let template = fs.readFileSync(`${__dirname}/../templates/${type}-meetup.md`, 'utf8');
+  for(let i = 0, keys = Object.keys(meetup), len = keys.length; i < len; i++) {
+    if(typeof meetup[keys[i]] === 'object') {
+      if(keys[i] === 'talks') {
+        const talks = [];
+        meetup[keys[i]].forEach(talk => {
+          talks.push(`- ${talk.type}: **${talk.title}**, [${talk.speaker}](${talk.url})`);
+        });
+        template = template.replace(`%talks%`, talks.join('\n'));
+      } else {
+        for(let j = 0, subKeys = Object.keys(meetup[keys[i]]), subLen = subKeys.length; j < subLen; j++) {
+          template = template.replace(`%${keys[i]}.${subKeys[j]}%`, meetup[keys[i]][subKeys[j]]);
+        } 
+      }
+    } else {
+      if(typeof keys[i] !== 'undefined') {
+        template = template.replace(`%${keys[i]}%`, meetup[keys[i]]);
+      }
+    }
+  }
+  saveEmail(template);
+}
+
+const saveEmail = (content) => {
+  const pathParams = process.argv[3].split('-');
+  mkdirp(`${__dirname}/../emails/${pathParams[0]}/${pathParams[1]}`, (err) => {
+    if (err) throw err;
+    fs.writeFileSync(`${__dirname}/../emails/${pathParams[0]}/${pathParams[1]}/${process.argv[2]}-meetup.md`, content, {encoding: 'utf8'});
+    console.log('Email creado!');
+  });
+};
+
+generateEmail(process.argv[2], process.argv[3]);
